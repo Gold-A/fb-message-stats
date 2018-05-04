636e87711d6cea3f455843e01be9fc3d0629bdfe
A <adrianmcheung@gmail.com>
2018-05-03 21:55:38 -0700

    Separate reaction graph into own class and add gitignore

---
+++ reactionGraph.py
@@ -0,0 +1,86 @@
+class ReactionGraph:
+    def __init__(self, msgs):
+        self._reactsByReactor = {}
+        self._reactsByReactee = {}
+        for msg in msgs:
+            for reaction in msg.getReactions():
+                self.addToReactionGraph(reaction)
+
+    def getReactsByReactee(self):
+        return self._reactsByReactee
+
+
+    def getReactsByReactor(self):
+        return self._reactsByReactor
+
+
+    def totalReacts(self, graph, usr, reaction):
+        count = 0
+        for s, reactions in graph[usr].iteritems():
+            if reaction in reactions:
+                count += reactions[reaction]
+        return count
+
+
+    def totalReactsReceived(self, speaker, reaction):
+        return self.totalReacts(self._reactsByReactee, speaker, reaction)
+
+
+    def totalReactsSent(self, reactor, reaction):
+        return self.totalReacts(self._reactsByReactor, reactor, reaction)
+
+
+    def addToReactionGraph(self, reactionObj):
+        self.addToReceiverGraph(reactionObj)
+        self.addToReactorGraph(reactionObj)
+
+
+    def addToReactorGraph(self, reactionObj):
+        reactor = reactionObj.getReactor()
+        speaker = reactionObj.getSpeaker()
+        reaction = reactionObj.getReaction()
+        if reactor not in self._reactsByReactor:
+            self._reactsByReactor[reactor] = {}
+        if speaker not in self._reactsByReactor[reactor]:
+            self._reactsByReactor[reactor][speaker] = {}
+        if reaction not in self._reactsByReactor[reactor][speaker]:
+            self._reactsByReactor[reactor][speaker][reaction] = 1
+        else:
+            self._reactsByReactor[reactor][speaker][reaction] += 1
+
+
+    def addToReceiverGraph(self, reactionObj):
+        reactor = reactionObj.getReactor()
+        speaker = reactionObj.getSpeaker()
+        reaction = reactionObj.getReaction()
+        if speaker not in self._reactsByReactee:
+            self._reactsByReactee[speaker] = {}
+        if reactor not in self._reactsByReactee[speaker]:
+            self._reactsByReactee[speaker][reactor] = {}
+        if reaction not in self._reactsByReactee[speaker][reactor]:
+            self._reactsByReactee[speaker][reactor][reaction] = 1
+        else:
+            self._reactsByReactee[speaker][reactor][reaction] += 1
+
+
+    def printReaction(self, rg, reaction):
+        self.printReactionNormalized(rg, None, reaction)
+
+
+    def printReactionNormalized(self, rg, hist, reaction):
+        graph = {}
+        if rg == "received":
+            graph = self._reactsByReactee
+        elif rg == "given":
+            graph = self._reactsByReactor
+        reactionHist = {}
+        for r, s in graph.iteritems():
+            reactionHist[r] = self.totalReacts(graph, r, reaction)
+        if hist == None:
+            for r, count in sorted(reactionHist.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+                if count > 0:
+                    print("%s: %s" % (r, count))
+        else:
+            for r, count in sorted(reactionHist.iteritems(), key=lambda (k,v): (float(v)/hist[k],k), reverse=True):
+                if count > 0:
+                    print("%s: %.2f%%" % (r, float(count*100)/hist[r]))
